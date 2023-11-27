0707a14f64c091fefcf9538615abaee4dcc3a1db
mgiorgi13 <96mat96@libero.it>
2023-11-27 17:28:40 +0100

    add simulation

---
+++ coap_network/fan/build/cooja/obj/.deps/random.d
@@ -0,0 +1,7 @@
+build/cooja/obj/random.o: ../../../../os/lib/random.c \
+ ../../../../os/services/rpl-border-router/module-macros.h \
+ ../../../../os/net/app-layer/coap/module-macros.h \
+ ../../../../os/lib/random.h
+../../../../os/services/rpl-border-router/module-macros.h:
+../../../../os/net/app-layer/coap/module-macros.h:
+../../../../os/lib/random.h:
