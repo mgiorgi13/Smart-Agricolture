089058ec023a3c2b7747b7c4d670142d1b08d4b3
mgiorgi13 <96mat96@libero.it>
2023-11-28 17:59:00 +0100

    add mqtt broker

--- monitoring_system/src/main/java/iot/unipi/it/Main.java
+++ monitoring_system/src/main/java/iot/unipi/it/Main.java
@@ -2,16 +2,31 @@ package iot.unipi.it;
 
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+import io.netty.handler.codec.mqtt.MqttMessage;
 
 public class Main {
     public static void main(String[] args) {
+        // COAP
+        // CoapClient client = new CoapClient("coap://fd00::202:2:2:2/leds?color=r");
 
-        CoapClient client = new CoapClient("coap://fd00::202:2:2:2/leds?color=r");
+        // String payload = "mode=on";
+        // CoapResponse response = client.put(payload, 0);
 
-        String payload = "mode=on";
-        CoapResponse response = client.put(payload, 0);	
-        
-        System.out.print(response.getResponseText());
+        // System.out.print(response.getResponseText());
+
+        // MQTT
+
+        try {
+            System.out.println("MQTT"); 
+            MQTThandler mc = new MQTThandler();
+        } catch (MqttException me) {
+            me.printStackTrace();
+        }
+
     }
-
 }
