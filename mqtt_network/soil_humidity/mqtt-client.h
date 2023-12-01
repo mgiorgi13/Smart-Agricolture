0c0842470d33b7da03a9c484ae7701660e361a45
mgiorgi13 <96mat96@libero.it>
2023-12-01 15:57:22 +0100

    fix mqtt comunication

--- mqtt_network/soil_humidity/mqtt-client.h
+++ mqtt_network/soil_humidity/mqtt-client.h
@@ -5,5 +5,10 @@
 /*---------------------------------------------------------------------------*/
 #include <string.h>
 
+#define DEFAULT_PUBLISH_INTERVAL    (30 * CLOCK_SECOND)
+#define DEFAULT_STATE_MACHINE_PERIODIC     (CLOCK_SECOND >> 1)
+static long PUBLISH_INTERVAL = DEFAULT_PUBLISH_INTERVAL;
+static long STATE_MACHINE_PERIODIC = DEFAULT_STATE_MACHINE_PERIODIC;
+
 #endif /* MQTT_CLIENT_H_ */
 /*---------------------------------------------------------------------------*/
